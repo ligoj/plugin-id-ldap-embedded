7808e015d224ee903b1f1fee20cd8846aaf8deac
Fabrice Daugan <fabrice.daugan@gfi.fr>
2018-01-31 00:04:53 +0100

    Update API to 2.1.0

--- src/test/java/org/ligoj/app/plugin/id/ldap/embedded/LdapEmbeddedTest.java
+++ src/test/java/org/ligoj/app/plugin/id/ldap/embedded/LdapEmbeddedTest.java
@@ -2,21 +2,21 @@ package org.ligoj.app.plugin.id.ldap.embedded;
 
 import javax.transaction.Transactional;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.ligoj.app.AbstractAppTest;
 import org.ligoj.app.model.Node;
 import org.ligoj.app.model.ParameterValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * Test class of {@link LdapEmbeddedResource}
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:/META-INF/spring/application-context-test.xml")
 @Rollback
 @Transactional
@@ -27,12 +27,12 @@ public class LdapEmbeddedTest extends AbstractAppTest {
 
 	@Test
 	public void getKey() {
-		Assert.assertEquals("service:id:ldap:embedded", resource.getKey());
+		Assertions.assertEquals("service:id:ldap:embedded", resource.getKey());
 	}
 
 	@Test
 	public void getInstalledEntities() {
-		Assert.assertTrue(resource.getInstalledEntities().contains(Node.class));
-		Assert.assertTrue(resource.getInstalledEntities().contains(ParameterValue.class));
+		Assertions.assertTrue(resource.getInstalledEntities().contains(Node.class));
+		Assertions.assertTrue(resource.getInstalledEntities().contains(ParameterValue.class));
 	}
 }
